0eed6ecd200ced5a459beb1c5356ef72907715d3
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-03 14:45:04 +0100

    Adding code to console log the seconds

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log('It works!');
-
-function setDate() {
-    console.log('Hi');
+function setDate(){
+const now = new Date();
+const seconds = now.getSeconds();
+console.log(seconds);
 }
 
 setInterval(setDate, 1000);
