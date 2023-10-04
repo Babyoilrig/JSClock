0f9b93e217e0e4a506d80ce0dcd5ffbcfda7c33a
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-04 19:44:55 +0100

    Adding console logs for the seconds and minutes

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const now = new Date();
 const seconds = now.getSeconds();
 const secondsDegrees = ((seconds / 60)* 360) + 90;
 secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-// console.log(seconds);
+console.log(`The seconds are ${seconds}`);
 
 const minutes = now.getMinutes();
-console.log(minutes);
+console.log(`The minutes are ${minutes}`);
 const minuteDegrees = ((minutes / 60)* 360) + 90;
 minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
