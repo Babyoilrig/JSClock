c76e08c9d8baa1cedd8c79574044afbdd42f7295
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-04 19:52:04 +0100

    Adding the code for the hour hand

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 // console.log(secondHand);
 console.log(minuteHand);
 function setDate(){
@@ -11,10 +12,14 @@ secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 console.log(`The seconds are ${seconds}`);
 //Code for minute hand
 const minutes = now.getMinutes();
-console.log(`The minutes are ${minutes}`);
+console.log(`The minute is ${minutes}`);
 const minuteDegrees = ((minutes / 60)* 360) + 90;
 minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-
+//Code for hour hand
+const hours = now.getHours();
+console.log(`The hour is ${hours}`);
+const hourDegrees = ((hours / 60)* 360) + 90;
+hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
