91cbf4c8f11d97ac9a87451937cc794664ca30cb
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-04 18:48:58 +0100

    Selecting the second hand and adding the rotation to it

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const secondHand = document.querySelector('.second-hand')
 function setDate(){
 const now = new Date();
 const seconds = now.getSeconds();
 const secondsDegrees = ((seconds / 60)* 360);
+secondHand.computedStyleMap.transform = `rotate(${secondsDegrees}deg)`;
 console.log(seconds);
 }
 
