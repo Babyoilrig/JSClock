edc70c8f90e0688f62bef2d684f323f3a8845ef2
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-04 19:58:16 +0100

    Correcting hour hand so it works correctly

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 //Code for hour hand
 const hours = now.getHours();
 console.log(`The hour is ${hours}`);
-const hourDegrees = ((hours / 60)* 360) + 90;
+const hourDegrees = ((hours / 12) * 360) + 90;
 hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
