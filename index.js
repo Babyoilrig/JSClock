59b128dee3f169091e90441446e6539ad5e3bb40
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-04 19:46:08 +0100

    Annotating code

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const minuteHand = document.querySelector('.min-hand');
 console.log(minuteHand);
 function setDate(){
 const now = new Date();
+//Code for seconds hand
 const seconds = now.getSeconds();
 const secondsDegrees = ((seconds / 60)* 360) + 90;
 secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 console.log(`The seconds are ${seconds}`);
-
+//Code for minute hand
 const minutes = now.getMinutes();
 console.log(`The minutes are ${minutes}`);
 const minuteDegrees = ((minutes / 60)* 360) + 90;
