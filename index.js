e2d55a4bc2397d1f37dba1dcbacb26825b4bcf64
babyoilrig <catrin.mentzoni@yahoo.com>
2023-10-09 21:14:14 +0100

    Commnting out the console logs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const seconds = now.getSeconds();
 const secondsDegrees = ((seconds / 60)* 360) + 90;
 secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-console.log(`The seconds are ${seconds}`);
+// console.log(`The seconds are ${seconds}`);
 //Code for minute hand
 const minutes = now.getMinutes();
 console.log(`The minute is ${minutes}`);
@@ -19,10 +19,10 @@ const minuteDegrees = ((minutes / 60)* 360) + 90;
 minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 //Code for hour hand
 const hours = now.getHours();
-console.log(`The hour is ${hours}`);
+// console.log(`The hour is ${hours}`);
 const hourDegrees = ((hours / 12) * 360) + 90;
 hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-//Fixing glith on all hands
+//Fixing glitch on all hands
 if(secondsDegrees === 90) {
     allHands.forEach(hand => hand.style.transition = 'none')
 } else {
